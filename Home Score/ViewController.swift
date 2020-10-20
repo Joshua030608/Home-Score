fe49e652af445c637267d8825de44d493fcdafee
Joshua030608 <64506906+Joshua030608@users.noreply.github.com>
2020-10-20 19:26:46 -0400

    Initial Commit

---
+++ Home Score/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Home Score
+//
+//  Created by Joshua Ford on 10/20/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
