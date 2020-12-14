00e3bfd93779f96fd9518cf0f416241a9ef8a593
Joshua030608 <64506906+Joshua030608@users.noreply.github.com>
2020-12-13 20:12:07 -0500

    Homework 12/8/20: Made categoryPicker show up and category cells show up.
    Made home reports clickable. Brings you to addEditHouseVC. Added categoryScorePicker. Fixed category cells. Now show up with proper scores. Added rows and components to picker view. Used delegate to give score using select row. Mostly works, need it to update scores on the housess and not go away so fast.

--- Home Score/Controller/AddEditHouseViewController.swift
+++ Home Score/Controller/AddEditHouseViewController.swift
@@ -11,20 +11,21 @@ class AddEditHouseViewController: UIViewController {
     @IBOutlet weak var houseImageView: UIImageView!
     @IBOutlet weak var addressTextField: UITextField!
     @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var categoryScorePicker: UIPickerView!
     @IBOutlet weak var tableView: UITableView!
     
     fileprivate var dataSource: CategoryScoresHouseDataSource?
-    
-    var home: Home? {
+    fileprivate var alreadyHaveData = false
+    var home: Home? { // Problem was home wasn't being set when was nil.
         didSet {
             if let home = home {
-                houseImageView.image = home.photos
+                /*
+                houseImageView.image = home.photos!
                 addressTextField.text = home.address
                 titleTextField.text = home.title
                 dataSource = CategoryScoresHouseDataSource(scoresDictionary: home.categoryScores)
-                
-            } else {
-                dataSource = CategoryScoresHouseDataSource(scoresDictionary: nil)
+ */
+            alreadyHaveData = true
             }
             DispatchQueue.main.async {
                 self.tableView.reloadData()
@@ -34,6 +35,61 @@ class AddEditHouseViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        if alreadyHaveData {
+            houseImageView.image = home!.photos
+            addressTextField.text = home!.address
+            titleTextField.text = home!.title
+            dataSource = CategoryScoresHouseDataSource(scoresDictionary: home!.categoryScores)
+        } else {
+            dataSource = CategoryScoresHouseDataSource(scoresDictionary: nil)
+        }
         tableView.dataSource = dataSource
+        tableView.delegate = self
+        categoryScorePicker.dataSource = self
+        categoryScorePicker.delegate = self
+        categoryScorePicker.isHidden = true
+    }
+}
+
+extension AddEditHouseViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let cell = tableView.cellForRow(at: indexPath) as! CategoryCell
+        let currentScore = cell.ratingLabel.text
+        let rowNumber: Int
+        if currentScore == "N/A" {
+            rowNumber = 0
+        } else {
+            let currentScoreInt = Int(currentScore!)
+            rowNumber = currentScoreInt!
+        }
+        categoryScorePicker.selectRow(rowNumber, inComponent: 0, animated: true)
+        categoryScorePicker.reloadAllComponents()
+        categoryScorePicker.isHidden = false
+    }
+}
+
+extension AddEditHouseViewController: UIPickerViewDataSource {
+func numberOfComponents(in pickerView: UIPickerView) -> Int {
+    return 1
+}
+
+func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+    return 11
+    }
+
+func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+    if row == 0 {
+        return "N/A"
+    } else {
+        let correctNumber = row
+        return String(correctNumber)
+        }
+    }
+}
+
+extension AddEditHouseViewController: UIPickerViewDelegate {
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        // Dissapears automatically because stopping on a row is selecting?
+        pickerView.isHidden = true
     }
 }
--- Home Score/Controller/ReportsOverviewViewController..swift
+++ Home Score/Controller/ReportsOverviewViewController..swift
@@ -28,6 +28,11 @@ class ReportsOverviewViewController: UIViewController {
 }
 
 extension ReportsOverviewViewController: UITableViewDelegate {
-    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let addEditHouseVC = UIStoryboard.init(name: "Main", bundle: nil).instantiateViewController(identifier: "AddEditHouseViewController") as! AddEditHouseViewController
+        let home1 = Home.savedHomes()[indexPath.row]
+        addEditHouseVC.home = home1
+        navigationController?.pushViewController(addEditHouseVC, animated: true)
+    }
 }
 
